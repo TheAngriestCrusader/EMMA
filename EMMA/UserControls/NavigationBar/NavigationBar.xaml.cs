a3b22808854e8fd5071786e7b3e3a5c804200ddf
The Angriest Crusader <theangriestcrusader@gmail.com>
2025-01-15 18:59:18 +0000

    Add NavigationBar UserControl.
    Basic controls implemented including a home button, a title text block, and a settings button.

---
+++ EMMA/UserControls/NavigationBar/NavigationBar.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace EMMA.UserControls.NavigationBar;
+
+public partial class NavigationBar : UserControl
+{
+    public NavigationBar()
+    {
+        InitializeComponent();
+    }
+}
