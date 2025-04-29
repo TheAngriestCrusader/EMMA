fe0d3f12589a484ce080a523dbe28e3d0b9cc932
TheAngriestCrusader <theangriestcrusader@gmail.com>
2025-04-29 09:34:46 +0100

    Create HomeButton_Click and SettingsButton_Click method to NavigationBar

--- EMMA/UserControls/NavigationBar/NavigationBar.xaml.cs
+++ EMMA/UserControls/NavigationBar/NavigationBar.xaml.cs
@@ -7,5 +7,17 @@ public partial class NavigationBar : UserControl
     public NavigationBar()
     {
         InitializeComponent();
+        HomeButton.Click += HomeButton_Click;
+        SettingsButton.Click += SettingsButton_Click;
+    }
+
+    private void HomeButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void SettingsButton_Click(object sender, System.Windows.RoutedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
